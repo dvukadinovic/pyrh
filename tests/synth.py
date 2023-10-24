40b797bf048b9ae50726c3f292721ca52193c1ea
Dusan Vukadinovic <vukadinovic@mps.mpg.de>
2023-10-24 23:56:47 +0200

    update in test example

--- tests/synth.py
+++ tests/synth.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pyrh
 from scipy.interpolate import interp1d
+from astropy.io import fits
 
 def spinor2multi(atm):
 	"""
@@ -31,6 +32,26 @@ def spinor2multi(atm):
 
 	return new
 
+def vacuum_to_air(wavelength, vacuum2air_limit=200.0000):
+    factor = np.ones_like(wavelength)
+    wave2 = 1/wavelength**2
+    factor[wavelength>vacuum2air_limit] = 1 + 2.735182e-4 + (1.314182 + 2.76249e4*wave2) * wave2
+
+    aux = wavelength/factor
+
+    return aux
+
+def air_to_vacuum(wavelength, air2vacuum_limit=199.9352):
+    sigma_sq = (1.0e7/wavelength)**2
+    fact = 1.0000834213 + 2.406030e6/(1.3e10 - sigma_sq) + 1.5997e4/(3.89e9 - sigma_sq)
+
+    ind = np.argmin(abs(wavelength-air2vacuum_limit))
+    fact[wavelength<air2vacuum_limit] = 1
+
+    aux = wavelength*fact
+
+    return aux
+
 atmos = np.loadtxt("falc.dat", skiprows=1)
 atmos = np.array(atmos.T, dtype=np.float64)
 atmos= spinor2multi(atmos)
@@ -48,47 +69,43 @@ mu = 1.0
 atm_scale = int(0)
 
 # wavelength grid in vacuum on which we compute the spectrum (in nm)
-wave = np.linspace(630.0, 630.35, num=251) + 0.2
+wave = np.linspace(630.2-0.15, 630.2+0.15, num=301)
+wave_vacuum = air_to_vacuum(wave)
 
 spec = pyrh.compute1d(cwd, mu, atm_scale, atmos,
-					  wave,
+					  wave_vacuum,
 					  int(0), np.empty(1, dtype=np.float64), np.empty((3,1), dtype=np.float64),
 					  np.array([], dtype=np.int32), np.array([], dtype=np.float64),
 					  np.array([], dtype=np.int32), np.array([], dtype=np.float64),
 					  False)
 spec = np.array(spec, dtype=np.float64)
+#np.savetxt("hinode_falc_pyrh.spec", spec.T, fmt="%7.6e")
 
+#--- create figure
 fig, ax = plt.subplots(nrows=2, ncols=1, 
 	sharex=True, height_ratios=[4,1])
 plt.subplots_adjust(hspace=0)
 
 #--- spectra
-
-ax[0].plot(wave, spec[0], 
-	label="synth", 
-	c="k")
+spec[0] /= spec[0,0]
+ax[0].plot(wave, spec[0], label="synth", c="k")
 
 hinode_pyrh = np.loadtxt("hinode_falc_pyrh.spec").T
-ax[0].plot(hinode_pyrh[-1], hinode_pyrh[0], 
-	label="pyrh benchamark",
-	c="tab:red")
+hinode_pyrh[0] /= hinode_pyrh[0,0]
+ax[0].plot(wave, hinode_pyrh[0], label="pyrh benchamark", c="tab:red")
 
-# hinode_pyrh = np.loadtxt("hinode_falc_spinor.spec").T
-# ax[0].plot(hinode_pyrh[-1], hinode_pyrh[0], label="SPINOR benchamark")
+spinor = fits.open("inverted_profs.1.fits")[0].data[0,0]
+spinor /= spinor[0,1]
+ax[0].plot(wave, spinor[0], label="SPINOR benchamark", c="tab:blue")
 
 #--- differences
-x = hinode_pyrh[-1]
-y = hinode_pyrh[0]
-if not np.allclose(wave, hinode_pyrh[-1], atol=1e-6, equal_nan=True):
-	x = wave
-	y = interp1d(hinode_pyrh[-1], y, kind="cubic")(x)
-ax[1].plot(x, spec[0] - y,
-	c="tab:red")
+ax[1].plot(wave, spec[0] - hinode_pyrh[0], c="tab:red")
+ax[1].plot(wave, spec[0] - spinor[0], c="tab:blue")
 
-ax[0].set_ylabel(r"Intensity [W Hz$^{-1}$ sr$^{-1}$]")
+ax[0].set_ylabel(r"Normalized intensity")
 ax[1].set_xlabel("Wavelength in vacuum [nm]")
 ax[1].set_xlim([wave[0], wave[-1]])
 
-ax[0].legend(frameon=True, loc="best")
+ax[0].legend(frameon=True, ncols=3, bbox_to_anchor=(0, 1.01, 1, 0.2),)
 
 plt.show()
