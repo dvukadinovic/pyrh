38326fd388ea74486d11e5baf785fa42d265b01e
Dusan Vukadinovic <vukadinovic@mps.mpg.de>
2024-09-18 15:07:14 +0200

    updated an example with comparison with Hamburg atlas spectrum

--- examples/synth.py
+++ examples/synth.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
 import pyrh
-import time
 
 def spinor2multi(atm):
 	"""
@@ -27,9 +27,20 @@ def spinor2multi(atm):
 	# magnetic field azimuth [rad]
 	new[7] = atm[-1]
 	# total hydrogen number density [1/cm3]
-	new[8] = (atm[3] - atm[4])/10/k/atm[2] / 1e6 / 1.26
+	new[8] = (atm[3] - atm[4])/10/k/atm[2] / 1e6 / 1.24
 
 	return new
+
+def air_to_vacuum(wavelength, air2vacuum_limit=199.9352):
+    sigma_sq = (1.0e7/wavelength)**2
+    fact = 1.0000834213 + 2.406030e6/(1.3e10 - sigma_sq) + 1.5997e4/(3.89e9 - sigma_sq)
+
+    ind = np.argmin(abs(wavelength-air2vacuum_limit))
+    fact[wavelength<air2vacuum_limit] = 1
+
+    aux = wavelength*fact
+
+    return aux
 
 atmos = np.loadtxt("falc.dat", skiprows=1)
 atmos = np.array(atmos.T, dtype=np.float64)
@@ -47,35 +58,25 @@ mu = 1.0
 # 2 -- height [km]
 atm_scale = int(0)
 
-# wavelength samples for which to compute the spectrum (in nm) in vacuum
-wave = np.linspace(630.0, 630.35, num=251) + 0.2
+# wavelength samples for which to compute the spectrum (in nm)
+wave_air = np.linspace(630.07, 630.3, num=251)
+wave = air_to_vacuum(wave_air)
 
-fig, axs = plt.subplots(nrows=2, ncols=1, sharex=True, height_ratios=[4,1])
-fig.subplots_adjust(hspace=0)
+spec = pyrh.compute1d(cwd, mu, atm_scale, atmos, wave)
+spec = np.array(spec, dtype=np.float64)
+spec[:4] /= spec[0,0]
 
-ids = 0
+from astropy.io import fits
+atlas = fits.open("/home/dusan/Documents/solar_atlases/Hamburg_atlas/FTS_Hamburg_center_normed_3290_8000.fits")[0].data
+atlas_wave = atlas[0] / 10 # [A --> nm]
+idl = np.argmin(np.abs(atlas_wave - 630.1))
+atlas_int_abs = atlas[1]/atlas[1,idl] # []
 
-old = None
-flag = [False, True]
-for idr in range(2):
-	# print("NLTE: ", flag[idr])
-	start = time.time()
-	spec = pyrh.compute1d(cwd, mu, atm_scale, atmos, wave, NLTE=flag[idr])
-	spec = np.array(spec, dtype=np.float64)
-	print(f"{time.time() - start:.3f}")
-	# print("===============================")
+plt.plot(wave_air, spec[0], color=f"C1")
+plt.plot(atlas_wave, atlas_int_abs)
 
-	axs[0].plot(wave, spec[ids]*1e8, label=idr+1, color=f"C{idr}")
-	if idr>0:
-		axs[1].plot(wave, (spec[ids] - old[ids])/spec[ids]*100, label=idr+1, color=f"C{idr}")
-	old = spec.copy()
-	# plt.plot(wave, (spec[0] - old[0])/spec[0]*100, label="new")
-	# plt.plot(old[-1], old[0]*1e8, label="old")
-
-axs[0].legend()
-axs[1].set_xlabel("Wavelength in vacuum [nm]")
-axs[0].set_ylabel(r"Intensity [10$^{-8}$ W/Hz/srad/m$^2$]")
-axs[1].set_ylabel(r"diff [\%]")
-axs[1].set_xlim([wave[0], wave[-1]])
+plt.xlabel("Wavelength in vacuum [nm]")
+plt.ylabel(r"Intensity [10$^{-8}$ W/Hz/srad/m$^2$]")
+plt.xlim([wave_air[0], wave_air[-1]])
 
 plt.show()
