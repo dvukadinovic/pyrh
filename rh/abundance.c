bf81d14b8e62f45ab64a7c0468b2674af34bab82
Dusan <vukadinovic.dusan.94@gmail.com>
2026-05-15 16:42:01 +0200

    added hard-coded abundance values from Asplund+21; values can be changed from globin

--- rh/abundance.c
+++ rh/abundance.c
@@ -61,6 +61,7 @@
 
 #define COMMENT_CHAR           "#"
 #define MINIMUM_LOG_ABUNDANCE  -8.0
+#define NELEMENTS              100
 
 /* --- Global variables --                             -------------- */
 
@@ -74,9 +75,36 @@ void readAbundance(Atmosphere *atmos, int Nabun, int *atomic_id, double *atomic_
   const char routineName[] = "readAbundance";
   register int n, k, i;
 
+  static double ABUNDANCE_ASPLUND21[NELEMENTS] = {12.0, 10.914, 
+                0.96, 1.38, 2.70, 8.46, 7.83, 8.69, 4.40, 8.06,
+                6.22, 7.55, 6.43, 7.51, 5.41, 7.12, 5.31, 6.38,
+                5.07, 6.30, 3.14, 4.97, 3.90, 5.62, 5.42, 7.46, 4.94, 6.20, 4.18, 4.56, 
+                3.02, 3.62, 2.30, 3.34, 2.54, 3.12,
+                2.32, 2.83, 2.21, 2.59, 1.47, 1.88, -7.96, 1.75, 0.78, 1.57, 0.96, 1.71,
+                0.80, 2.02, 1.01, 2.18, 1.55, 2.22,
+                1.08, 2.27,
+                1.11, 1.58, 0.78, 1.42, -7.96, 0.95, 0.52, 1.08, 0.31, 1.1, 0.48, 0.93, 0.11, 0.85, 0.10, 
+                0.85, -0.15, 0.79, 0.26, 1.35, 1.32, 1.61, 0.91, 1.17, 0.92, 1.95, 0.65, -7.96, -7.96, -7.96,
+                -7.96, -7.96,
+                -7.96, 0.03, -7.96, -0.54, -7.96, -7.96, -7.96, -7.96, -7.96, -7.96, -7.96, -7.96};
+
+  static char* ELEMENTS_ID[NELEMENTS] = {"H",  "He", 
+                "Li", "Be", "B",  "C",  "N",  "O",  "F",  "Ne",
+                "Na", "Mg", "Al", "Si", "P",  "S",  "Cl", "Ar",
+                "K",  "Ca", "Sc", "Ti", "V",  "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn",
+                "Ga", "Ge", "As", "Se", "Br", "Kr",
+                "Rb", "Sr", "Y",  "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd",
+                "In", "Sn", "Sb", "Te", "I",  "Xe",
+                "Cs", "Ba", 
+                "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu",
+                "Hf", "Ta", "W",  "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "At", "Rn",
+                "Fr", "Ra",
+                "Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm"};
+
   char   ID[ATOM_ID_WIDTH+1], line[MAX_LINE_SIZE], *match;
   bool_t result = TRUE, DEX = FALSE, exit_on_EOF;
-  int    Nread, pti, ida_reading=1;
+  int    Nread, pti;
+  // int    ida_reading=1;
   double abund, totalAbund, avgWeight, metallicity;
   Element *element;
   FILE  *fp_abund, *fp_pf;
@@ -98,47 +126,75 @@ void readAbundance(Atmosphere *atmos, int Nabun, int *atomic_id, double *atomic_
     element->model = NULL;
   }
 
-  if ((fp_abund = fopen(input.abund_input, "r")) == NULL) {
-    sprintf(messageStr,
-	    "Unable to open input file %s", input.abund_input);
-    Error(ERROR_LEVEL_2, routineName, messageStr);
-  }
-  /* --- Read abundances from file --                  -------------- */
+  // if ((fp_abund = fopen(input.abund_input, "r")) == NULL) {
+  //   sprintf(messageStr,
+	//     "Unable to open input file %s", input.abund_input);
+  //   Error(ERROR_LEVEL_2, routineName, messageStr);
+  // }
+  // /* --- Read abundances from file --                  -------------- */
 
-  while (getLine(fp_abund, COMMENT_CHAR, line, exit_on_EOF=FALSE) != EOF) {
-    if ((Nread = sscanf(line, "%s %lf", ID, &abund)) != 2) {
-      sprintf(messageStr, "Unable to read input file %s",
-	      input.abund_input);
-      Error(ERROR_LEVEL_2, routineName, messageStr);
-    }
-    UpperCase(ID);
+  // while (getLine(fp_abund, COMMENT_CHAR, line, exit_on_EOF=FALSE) != EOF) {
+  //   if ((Nread = sscanf(line, "%s %lf", ID, &abund)) != 2) {
+  //     sprintf(messageStr, "Unable to read input file %s",
+	//       input.abund_input);
+  //     Error(ERROR_LEVEL_2, routineName, messageStr);
+  //   }
+  //   UpperCase(ID);
+  //   if (strlen(ID) == 1) strcat(ID, " ");
+
+  //   // update abundance to those forwarded from globin
+  //   if (Nabun!=0){
+  //     for (int ida=0; ida<Nabun; ida++){
+  //       if (atomic_id[ida]==ida_reading){
+  //         abund = atomic_abundance[ida];
+  //         break;
+  //       }
+  //     }
+  //   }
+  //   ida_reading += 1;
+
+  //   for (n = 0;  n < atmos->Nelem;  n++) {
+  //     if ((match = strstr(atmos->elements[n].ID, ID))) {
+  //       atmos->elements[n].abund = abund;
+  //       if (strstr(ID, "H ")  &&  (abund == 12.0)) DEX = TRUE;
+  //             atmos->elements[n].abundance_set = TRUE;
+  //       break;
+  //     }
+  //   }
+  //   if (!match) {
+  //     sprintf(messageStr, "Abundance for element %s not used", ID);
+  //     Error(WARNING, routineName, messageStr);
+  //   }
+  // }
+  // fclose(fp_abund);
+
+  for (int ide=0; ide<NELEMENTS; ide++){
+    strcpy(ID, ELEMENTS_ID[ide]);
+    abund = ABUNDANCE_ASPLUND21[ide];
     if (strlen(ID) == 1) strcat(ID, " ");
+    UpperCase(ID);
 
     // update abundance to those forwarded from globin
     if (Nabun!=0){
       for (int ida=0; ida<Nabun; ida++){
-        if (atomic_id[ida]==ida_reading){
+        // if (atomic_id[ida]==ida_reading){
+        if (atomic_id[ida]==ide+1){
           abund = atomic_abundance[ida];
           break;
         }
       }
     }
-    ida_reading += 1;
+    // ida_reading += 1;
 
     for (n = 0;  n < atmos->Nelem;  n++) {
       if ((match = strstr(atmos->elements[n].ID, ID))) {
         atmos->elements[n].abund = abund;
         if (strstr(ID, "H ")  &&  (abund == 12.0)) DEX = TRUE;
-              atmos->elements[n].abundance_set = TRUE;
+        atmos->elements[n].abundance_set = TRUE;
         break;
       }
     }
-    if (!match) {
-      sprintf(messageStr, "Abundance for element %s not used", ID);
-      Error(WARNING, routineName, messageStr);
-    }
   }
-  fclose(fp_abund);
 
   /* --- Multiply with metallicity factor if different from 1.0 -- -- */
 
